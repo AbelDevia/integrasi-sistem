86adcb115033b5987ad494684784262ea964db0a
AbelDevia <abeldeviaagustin23@mhs.politala.ac.id>
2024-11-05 08:20:21 +0800

    merge

--- resources/views/homepage/index.blade.php
+++ resources/views/homepage/index.blade.php
@@ -5,7 +5,7 @@
             <div class="item item-1">
                 <div class="header-text">
                     <span class="category">Toronto, <em>Canada</em></span>
-                    <h2>Hurry!<br>Get the Best Villa for you</h2>
+                    <h2>Selamat Datang<br>Sistem Pakar Diagnosa Penyakit Pada Hewan Kambing</h2>
                 </div>
             </div>
             <div class="item item-2">
